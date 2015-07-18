840ff30593f080d00c4537c34d4deea7edb6f999
florin.nichifiriuc <florinnichifiriuc@gmail.com>
2015-07-18 16:07:54 +0300

    Add task

--- client/views/addtask/addtask.js
+++ client/views/addtask/addtask.js
@@ -1,3 +1,6 @@
-Template.addtask.rendered = function() {
-
-};
+Template.addtask.events ({
+    'click btn.submit': function () {
+        var form = document.getElementById("formID").value;
+        tasks.insert(form);
+    }
+});
