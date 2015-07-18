54c1e5dc85cc5d4633dcc3581c5b66adf19cb8c0
florin.nichifiriuc <florinnichifiriuc@gmail.com>
2015-07-18 21:02:19 +0300

    Add planning

--- client/views/planningpoker/planningpoker.js
+++ client/views/planningpoker/planningpoker.js
@@ -1,13 +1,21 @@
-Template.dashboard.rendered = function() {
+Template.dashboard.rendered = function () {
 
 };
 
 Template.planningpoker.helpers({
-    chatMessages: function(){
+    chatMessages: function () {
         return chatSessions.find({}, {sort: {createdAt: -1}});
     },
+   voteMessages: function () {
+        return voteSessions.find({}, {sort: {createdAt: -1}});
+    },
+    storyNumbers: function () {
+        var tasks = Session.get('tasksIds') || [],
+            currentTask = Session.get('currentSite') || 1;
+        return currentTask + '/' + tasks.length;
+    },
 
-    moreThanZero: function(messages) {
+    moreThanZero: function (messages) {
 
         if (messages.length > 0) {
             return true;
@@ -19,22 +27,33 @@ Template.planningpoker.helpers({
 });
 
 
-
-
 Template.planningpoker.events({
-    'submit #chatSubmit':function(e){
+    'submit #chatSubmit': function (e) {
         e.preventDefault();
         var a = {
-            name: 'Alex',
+            name: Meteor.user().emails[0].address,
             message: $('#enterMessage').val(),
             planningSessionId: Session.get('planningSessionId') || "0",
-            taskId :Session.get('taskId') || "0",
+            taskId: Session.get('taskId') || "0",
             createdAt: new Date()
 
         };
         $('#enterMessage').val("");
         chatSessions.insert(a);
         return false;
-    }
+    },
+    'click .card2': function (e) {
+        e.preventDefault();
+        var a = {
+            name: Meteor.user().emails[0].address,
+            value: $(e.target).data('value'),
+            planningSessionId: Session.get('planningSessionId') || "0",
+            taskId: Session.get('taskId') || "0",
+            createdAt: new Date()
+
+        };
+        voteSessions.insert(a);
+        return false;
+    },
 
 });
