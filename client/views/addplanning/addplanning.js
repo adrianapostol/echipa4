db5ad1ef3c9e5b930b6795eb5021285180c5594c
florin.nichifiriuc <florinnichifiriuc@gmail.com>
2015-07-18 20:42:24 +0300

    Add planning

--- client/views/addplanning/addplanning.js
+++ client/views/addplanning/addplanning.js
@@ -20,15 +20,36 @@ Template.addplanning.events({
     }
 });
 
+
+planningSessions.before.insert(function (userId, doc) {
+    doc.tasks = [];
+    doc.participants = [];
+    jQuery('input.participants[type=hidden]').each(function () {
+        doc.participants.push(this.value);
+    });
+    jQuery('input.tasks[type=hidden]').each(function () {
+        doc.tasks.push(this.value);
+    });
+    debugger;
+
+    doc.createdAt = moment().toDate();
+    doc.createdBy = userId;
+});
+
 AutoForm.addHooks('insertPlanningSession', {
     onSuccess: function (operation, result) {
         Router.go('planninglist');
     },
+    beforeSubmit: function () {
+
+    },
     onSubmit: function (insertDoc, updateDoc, currentDoc) {
+        debugger;
         console.log(arguments);
         return false;
     },
     onError: function (insertDoc, updateDoc, currentDoc) {
+        console.log(arguments);
         alert('A error has occured please try again later!');
         return false;
     }
